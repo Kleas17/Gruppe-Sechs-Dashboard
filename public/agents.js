d43112c246d9ea7f66e6cc046e54a095993d30e9
Da Mota <kleas3.marc@gmail.com>
2023-10-01 16:15:15 +0200

    test bdd

--- public/agents.js
+++ public/agents.js
@@ -1,4 +1,21 @@
-document.getElementById('addAgentBtn').addEventListener('click', function() {
+// Importation des modules nécessaires de Firebase
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, set, get, remove } from "firebase/database";
+
+// Configuration et initialisation de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDmhVVLDbhScGK6DaIj81LHE5WUBWRpSEU",
+    authDomain: "gruppe-sechs.firebaseapp.com",
+    databaseURL: "https://gruppe-sechs-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gruppe-sechs",
+    storageBucket: "gruppe-sechs.appspot.com",
+    messagingSenderId: "726047862310",
+    appId: "1:726047862310:web:109125c4b5b70b702c00e7"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+document.getElementById('addAgentBtn').addEventListener('click', function () {
     const agentName = document.getElementById('agentName').value;
     if (agentName) {
         const availability = {};
@@ -12,43 +29,48 @@ document.getElementById('addAgentBtn').addEventListener('click', function() {
     }
 });
 
-
 function addAgent(agent) {
-    const agents = getAgents();
-    agents.push(agent);
-    localStorage.setItem('agents', JSON.stringify(agents));
-    displayAgents();
+    const agentRef = push(ref(db, 'agents'));
+    set(agentRef, agent).then(() => displayAgents());
 }
 
-function getAgents() {
-    const agents = localStorage.getItem('agents');
-    return agents ? JSON.parse(agents) : [];
+async function getAgents() {
+    const agentsRef = ref(db, 'agents');
+    const agentsSnap = await get(agentsRef);
+    if (agentsSnap.exists()) {
+        return Object.entries(agentsSnap.val()).map(([key, value]) => ({
+            id: key,
+            ...value
+        }));
+    } else {
+        return [];
+    }
 }
 
-function displayAgents() {
-    const agents = getAgents();
+async function displayAgents() {
+    const agents = await getAgents();
     const agentList = document.getElementById('agentList');
     agentList.innerHTML = '';
-    agents.forEach((agent, index) => {
+    agents.forEach(agent => {
         const agentItem = document.createElement('div');
         agentItem.className = 'agent-item';
         agentItem.innerHTML = `
             ${agent.name} 
-            <button onclick="deleteAgent(${index})">X</button>
+            <button onclick="deleteAgent('${agent.id}')">X</button>
             <div class="availability">
-                ${Object.entries(agent.availability).map(([day, avail]) => `<p>${day}: ${avail}</p>`).join('')}
+                ${Object.entries(agent.availability).map(([day, avail]) => `<p>${day}: ${avail.debut} - ${avail.fin}</p>`).join('')}
             </div>
         `;
         agentList.appendChild(agentItem);
     });
 }
 
-function deleteAgent(index) {
-    const agents = getAgents();
-    agents.splice(index, 1);
-    localStorage.setItem('agents', JSON.stringify(agents));
-    displayAgents();
+window.deleteAgent = function (id) {
+    const agentRef = ref(db, 'agents/' + id);
+    remove(agentRef).then(() => displayAgents());
 }
 
 // Affiche les agents au chargement de la page
-window.onload = displayAgents;
+window.onload = async () => {
+    await displayAgents();
+};
